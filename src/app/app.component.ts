32785673f45f1341ce1b257742dd865ec1f14eb6
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2019-11-06 11:27:46 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'obs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'unicorn-ng';
+}
